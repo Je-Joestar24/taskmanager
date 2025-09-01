c2d65c3ae28a28614b8715dce5a6972dee884ff6
Jejomar Parrilla <jpar1252003@gmail.com>
2025-09-01 18:40:36 +0800

    backend: feat(auth): add UserResources, UserSeeder and api

---
+++ backend/database/seeders/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Create Admin User
+        User::updateOrCreate(
+            ['email' => 'admin@example.com'],
+            [
+                'name' => 'Admin User',
+                'password' => Hash::make('admin123'),
+                'role' => 'admin',
+            ]
+        );
+
+        // Create Normal Users
+        User::factory()->count(5)->create([
+            'role' => 'user',
+        ]);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/test', function (Request $request) {
     return  response()->json(app()->version());
+});
+
+Route::post('/login', [LoginController::class, 'login']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [LoginController::class, 'logout']);
 });
